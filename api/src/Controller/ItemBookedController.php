f26556112c38ebc178fcaf83e690644607550f46
Vlad S <v.sadkov@andersenlab.com>
2022-07-31 22:18:11 +0300

    Added Validator

--- api/src/Controller/ItemBookedController.php
+++ api/src/Controller/ItemBookedController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Controller\Traits\DatePeriodFromRequestTrait;
+use App\Controller\Traits\ItemAvailabilityValidationTrait;
 use App\Repository\ItemAvailabilityRepository;
 use Doctrine\DBAL\Exception;
 use Doctrine\ORM\NonUniqueResultException;
@@ -16,14 +17,20 @@ use Symfony\Component\HttpKernel\Attribute\AsController;
 #[AsController]
 class ItemBookedController extends AbstractController
 {
-    use DatePeriodFromRequestTrait;
+    use DatePeriodFromRequestTrait, ItemAvailabilityValidationTrait;
 
     /**
-     * @throws Exception|NonUniqueResultException
+     * @throws Exception|NonUniqueResultException|\Exception
      */
     public function __invoke(Request $request, ItemAvailabilityRepository $itemAvailabilityRepository): JsonResponse
     {
         $stationId = (int) $request->query->get('station');
+
+        $errors = $this->validate($request);
+
+        if ($errors->count() > 0) {
+            return $this->json($errors, 400);
+        }
 
         $period = $this->getPeriod($request);
 
